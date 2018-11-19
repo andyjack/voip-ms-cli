3ee49b0dfcacdb812364485260d310a67f68e3e9
Andy Jack <github@veracity.ca>
2018-11-19 12:30:16 -0500

    Finish reading the bash completion tutorial

--- voip-ms-cli_completion.bash
+++ voip-ms-cli_completion.bash
@@ -2,6 +2,9 @@
 # https://iridakos.com/tutorials/2018/03/01/bash-programmable-completion-tutorial.html
 _voipmscli_completions()
 {
+  if [ "${#COMP_WORDS[@]}" != "2" ]; then
+    return
+  fi
   COMPREPLY=($(compgen -W "show-balance show-recent block-recent block-number" "${COMP_WORDS[1]}"))
 }
 
